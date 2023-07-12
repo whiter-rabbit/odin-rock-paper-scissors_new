32a1600bbca47fe64184dd639cd99cf5dae5680d
whiter-rabbit <126683095+whiter-rabbit@users.noreply.github.com>
2023-07-12 14:51:53 +0200

    Amend function'game' working with visual interface
    Single round is played with pressing a button, shows result below.
    ISSUE: game still works for singleRound. Swapping to the function  with
    more rounds ends up with playersChoice being all the time the same.

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-
-function getComputerChoice() {
-    computerNumber = (Math.floor(Math.random() * 3)) + 1;
-    if (computerNumber === 1) {
-        computerChoice = "rock";
-    }
-    else if (computerNumber === 2) {
-        computerChoice = "paper"
-    }
-    else {
-        computerChoice = "scissors"
-    }
-    return computerChoice;
-}
-
 const rockBtn = document.getElementById('rock-btn');
 const paperBtn = document.getElementById('paper-btn');
 const scissorsBtn = document.getElementById('scissors-btn');
 
-const playBtn = document.getElementById("playBtn");
+const result = document.querySelector(".result");
+const scores = document.querySelector(".scores");
 
 rockBtn.addEventListener("click", function(e) {
     rockBtn.classList.add("chosen");
@@ -39,23 +25,6 @@ scissorsBtn.addEventListener("click",  function(e) {
 });
 
 
-function playerSelection() {
-    if (rockBtn.classList.contains("chosen")) {
-        return "rock"
-    }
-    else if (paperBtn.classList.contains("chosen")) {
-        return "paper"
-    }
-    else if (scissorsBtn.classList.contains("chosen")) {
-        return "scissors"
-    };
-
-
-    console.log("playerChoice = " + playerChoice);
-    console.log(typeof playerChoice);
-    
-}
-
 const win = ["win", 1];
 const lose = ["lose", -1];
 const tie = ["tie", 0];
@@ -65,52 +34,129 @@ function singleRound() {
     let a = playerSelection();
     let b = getComputerChoice();
 
+    /*
+    // BELOW: creates a new node that will appear above the one with result (as a first child). For adding future results.
+    const resultNew = document.createElement('div'); 
+    resultNew.classList.add('result'); 
+    scores.prepend(resultNew)
+    resultNew.innerText ="Test";
+    */
+
     if (a === "rock" && b === "rock") {
-        console.log("Tie! Rock vs Rock");
+        result.innerText ="Tie! Rock vs Rock";
+        rockBtn.classList.toggle("chosen", false);
         return tie;
+        
     }
     else if (a === "rock" && b === "paper") {
-        console.log("You Lose! Rock loses against Paper");
+        result.innerText = "You Lose! Rock loses against Paper";
+        rockBtn.classList.toggle("chosen", false);
         return lose;
     }
     else if (a === "rock" && b === "scissors") {
-        console.log("You Win! Rock beats Scissors");
+        result.innerText = "You Win! Rock beats Scissors";
+        rockBtn.classList.toggle("chosen", false);
         return win;
     }
     else if (a === "paper" && b === "rock") {
-        console.log("You Win! Paper beats Rock");
+        result.innerText = "You Win! Paper beats Rock";
+        paperBtn.classList.toggle("chosen", false);
         return win;
     }
     else if (a === "paper" && b === "paper") {
-        console.log("Tie! Paper vs Paper");
+        result.innerText = "Tie! Paper vs Paper";
+        paperBtn.classList.toggle("chosen", false);
         return tie;
     }
     else if (a === "paper" && b === "scissors") {
-        console.log("You Lose! Paper loses against Scissors");
+        result.innerText = "You Lose! Paper loses against Scissors";
+        paperBtn.classList.toggle("chosen", false);
         return lose;
     }
     else if (a === "scissors" && b === "rock") {
-        console.log("You Lose! Scissors lose against Rock");
+        result.innerText = "You Lose! Scissors lose against Rock";
+        scissorsBtn.classList.toggle("chosen", false);
         return lose;
     }
     else if (a === "scissors" && b === "paper") {
-        console.log("You Win! Scissors beats Paper");
+        result.innerText = "You Win! Scissors beats Paper";
+        scissorsBtn.classList.toggle("chosen", false);
         return win;
     }
     else if (a === "scissors" && b === "scissors") {
-        console.log("Tie! Scissors vs Scissors");
+        result.innerText = "Tie! Scissors vs Scissors";
+        scissorsBtn.classList.toggle("chosen", false);
         return tie;
     }
-    // else {
-    //     alert("Error. Type: Rock, Paper or Scissors");
-    //     return singleRound();
-    // }
 }
 
-//INFO - below WORKING 5 ROUNDS GAME TO BE BROUGHT BACK 
+
+function getComputerChoice() {
+    computerNumber = (Math.floor(Math.random() * 3)) + 1;
+    if (computerNumber === 1) {
+        computerChoice = "rock";
+    }
+    else if (computerNumber === 2) {
+        computerChoice = "paper"
+    }
+    else {
+        computerChoice = "scissors"
+    }
+    return computerChoice;
+}
+
+
+function playerSelection() {
+    if (rockBtn.classList.contains("chosen")) {
+        return "rock"
+    }
+    else if (paperBtn.classList.contains("chosen")) {
+        return "paper"
+    }
+    else if (scissorsBtn.classList.contains("chosen")) {
+        return "scissors"
+    }; 
+}
 
 function game () {
-    //alert("Play 5 rounds of the Rock - Paper - Scissors game to see if you can win with the computer");
+
+}
+
+
+
+
+
+function game () {
+
+    const totalOutput =[];
+    let partialOutput;
+    let totalScore = 0;
+  
+    for (let i = 0; i < 5; i++) {
+      partialOutput = singleRound();
+      totalOutput.push(partialOutput[0]);
+      totalScore = totalScore + partialOutput[1];
+    }
+      
+      if (totalScore > 0) {
+          alert("You've WON. Your score is: " + totalOutput)
+      }
+      
+      else if (totalScore === 0) {
+          alert("You've TIED. Your score is: " + totalOutput)
+      }
+  
+      else if (totalScore < 0) {
+          alert("You've LOST. Your score is: " + totalOutput)
+      }
+  }
+
+    
+  
+/*
+//INFO - below WORKING old 5 ROUNDS GAME 
+
+function game () {
 
   const totalOutput =[];
   let partialOutput;
@@ -134,11 +180,7 @@ function game () {
         alert("You've LOST. Your score is: " + totalOutput)
     }
 }
-
-
-//game();
-
-
+*/
 
 /*
 // INFO : below WORKING temporary 1 ROUND GAME 
