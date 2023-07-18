132ec6201f6c8b75338a319c1490b16fa83f79b4
whiter-rabbit <126683095+whiter-rabbit@users.noreply.github.com>
2023-07-18 10:55:20 +0100

    Fix - all partial results show below
    Issue noticed - it's necessary to click button 6th time to see the notice of final result.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const paperBtn = document.getElementById('paper-btn');
 const scissorsBtn = document.getElementById('scissors-btn');
 
 const scores = document.querySelector(".scores");
+let score = document.querySelector(".score")
 const result = document.querySelector(".result");
 
 let gamesPlayed = 0
@@ -38,13 +39,95 @@ let totalScore = 0
 function singleRound() {
     let a = playerSelection();
     let b = getComputerChoice();
+
+    score = document.createElement('p');
+    scores.prepend(score);
+    
+    if (gamesPlayed <= 5 )  {
+        
+       
+        if (a === "rock" && b === "rock") {
+            scores.prepend(score);
+            score.innerText ="Tie! Rock vs Rock";
+            rockBtn.classList.toggle("chosen", false);
+            totalScore = totalScore + 0;
+            return totalScore;
+            
+        }
+        else if (a === "rock" && b === "paper") {
+            scores.prepend(score);
+            score.innerText = "You Lose! Rock loses against Paper";
+            rockBtn.classList.toggle("chosen", false);
+            totalScore = totalScore - 1;
+            return totalScore;
+        }
+        else if (a === "rock" && b === "scissors") {
+            scores.prepend(score);
+            score.innerText = "You Win! Rock beats Scissors";
+            rockBtn.classList.toggle("chosen", false);
+            totalScore = totalScore + 1;
+            return totalScore;
+        }
+        else if (a === "paper" && b === "rock") {
+            scores.prepend(score);
+            score.innerText = "You Win! Paper beats Rock";
+            paperBtn.classList.toggle("chosen", false);
+            totalScore = totalScore + 1;
+            return totalScore;
+        }
+        else if (a === "paper" && b === "paper") {
+            scores.prepend(score);
+            score.innerText = "Tie! Paper vs Paper";
+            paperBtn.classList.toggle("chosen", false);
+            totalScore = totalScore + 0;
+            return totalScore;
+        }
+        else if (a === "paper" && b === "scissors") {
+            scores.prepend(score);
+            score.innerText = "You Lose! Paper loses against Scissors";
+            paperBtn.classList.toggle("chosen", false);
+            totalScore = totalScore - 1;
+            return totalScore;
+        }
+        else if (a === "scissors" && b === "rock") {
+            scores.prepend(score);
+            score.innerText = "You Lose! Scissors lose against Rock";
+            scissorsBtn.classList.toggle("chosen", false);
+            totalScore = totalScore - 1;
+            return totalScore;
+        }
+        else if (a === "scissors" && b === "paper") {
+            scores.prepend(score);
+            score.innerText = "You Win! Scissors beats Paper";
+            scissorsBtn.classList.toggle("chosen", false);
+            totalScore = totalScore + 1;
+            return totalScore;
+        }
+        else if (a === "scissors" && b === "scissors") {
+            scores.prepend(score);
+            score.innerText = "Tie! Scissors vs Scissors";
+            scissorsBtn.classList.toggle("chosen", false);
+            totalScore = totalScore + 0;
+            return totalScore;
+        }
+    }
+    else {
+        gameScore();
+    }
+}
+
+
+/*
+function singleRound() {
+    let a = playerSelection();
+    let b = getComputerChoice();
     
     // BELOW: creates a new node that will appear above the one with 
     //result (as a first child). For adding future results.
-    //const resultNew = document.createElement('div'); 
-    //resultNew.classList.add('result'); 
-    //scores.prepend(resultNew)
-    //resultNew.innerText ="Test";
+    // const resultNew = document.createElement('div'); 
+    // resultNew.classList.add('result'); 
+    // scores.prepend(resultNew)
+    // resultNew.innerText ="Test";
     
     if (gamesPlayed < 5)  {
         
@@ -110,6 +193,7 @@ function singleRound() {
     }
 }
 
+*/
 
 function getComputerChoice() {
     computerNumber = (Math.floor(Math.random() * 3)) + 1;
@@ -144,18 +228,15 @@ function playerSelection() {
     console.log(totalScore);
 
     if (totalScore > 0) {
-        //result.innerText ="You've WON";
-        console.log("You've WON")
+        result.innerText = "You've WON";
     }
     
     else if (totalScore === 0) {
-        //result.innerText = "You've TIED";
-        console.log("You've TIED")
+        result.innerText = "You've TIED";
     }
 
     else if (totalScore < 0) {
-        //result.innerText ="You've LOST";
-        console.log("You've TIED")
+        result.innerText = "You've LOST";
     }
   }
 
